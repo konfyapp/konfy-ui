bf3e9dfc26aeb44d2c875d8e8472543ec5cc3476
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2018-08-23 13:54:28 +0200

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<file-tray></file-tray>
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import FileTray from '@/components/vue-file-tray'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+	name: 'home',
+	components: {
+		HelloWorld, FileTray
+	}
 }
 </script>
